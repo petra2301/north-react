a6bf151fb2b05aa68e84ccf8dbe11406e9112213
petra2301 <hello@petragergely.dk>
2022-08-27 17:25:51 +0200

    successfully fetching places from restdb

---
+++ north-react/src/App.js
@@ -0,0 +1,46 @@
+import React, { Component } from "react";
+import ReactDOM from 'react-dom';
+import './css/style.css';
+import './css/components.css';
+
+import Norway from './components/Norway'
+
+class App extends Component {
+  state = {
+    value: "Choose a country"
+  }
+
+  selectCountryHandler = (event) => {
+    this.setState({
+      value: event.target.value
+    })
+    ReactDOM.render(<Norway />, document.getElementById('places'));
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <h1>GoNorth bucketlist app</h1>
+        <label>Choose a country:</label>
+        <select name="countrySelect" id="countrySelect" ref="countrySelect"
+          onChange={this.selectCountryHandler} >
+          <option hidden disabled selected>Select a country...</option>
+          <option value="Denmark">Denmark</option>
+          <option value="FaroeseIslands">Faroese islands</option>
+          <option value="Finland">Finland</option>
+          <option value="Greenland">Greenland</option>
+          <option value="Iceland">Iceland</option>
+          <option value="Norway">Norway</option>
+          <option value="Scotland">Scotland</option>
+          <option value="Sweden">Sweden</option>
+        </select>
+        <button>Let's go!</button>
+
+        <p>The selected country is {this.state.value}.</p>
+        <div id="places"></div>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ north-react/src/components/Norway.js
@@ -0,0 +1,77 @@
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+
+class Norway extends Component {
+
+    state = {
+        error: null,
+        isLoaded: false,
+        places: []
+    };
+
+    noSuggestionErrorMessage = "We don't have any suggestion for this. Try another activity, or get some inspiration from our premade trips.";
+
+    componentDidMount() {
+            
+            fetch("https://bucketlist-cd32.restdb.io/rest/norway", {
+                method: "get",
+                headers: {
+                  "Content-Type": "application/json; charset=utf-8",
+                  "x-apikey": "	6308ba876ca2e567447720de",
+                  "cache-control": "no-cache"
+                }
+            })
+          .then(res => res.json())
+          .then(
+            (result) =>
+             {
+                 this.setState({
+                     isLoaded: true,
+                     places: result
+                   });
+                 }
+                ,
+                 (error) => {
+                     this.setState({
+                       isLoaded: true,
+                       error
+                     });
+                   },
+                )
+        }
+
+
+
+
+        
+
+    render() {
+        const { error, isLoaded, places } = this.state;
+
+        if (error) {
+            return <div>Error: {error.message}</div>;
+        } else if (!isLoaded) {
+            return <div><p className="centered">Loading...</p></div>;
+        } else {
+            let itemContainer = [];
+          for (let i = 0; i < this.state.places.length; i++) {
+            let data = {
+              "id": this.state.places[i]._id,
+              "name": this.state.places[i].name,
+              "imgUrl": this.state.places[i].imgUrl
+            }            
+              itemContainer.push(places);
+            }
+
+            return (
+                //console.log(this.state.places[1].name)
+                <div>
+                    {places.map(place => <p>{place.name}</p>)}
+                </div>
+              );
+
+            }
+          }
+        }
+
+export default Norway
